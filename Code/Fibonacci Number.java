fbafd6cba6a4027452aeaf94c15990af7eca4d4a
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2019-11-24 14:54:44 +0800

    Create Fibonacci Number.java

---
+++ Code/Fibonacci Number.java
@@ -0,0 +1,14 @@
+public int fib(int n) {
+        int result =0,a=0,b=1;
+        if (n==0)
+            return 0;
+        else if (n==1)
+            return 1;
+        for (int i=2;i<=n;i++){
+            result = a+b;
+            a=b;
+            b=result;
+        }
+        return result;
+    }
+// can use on climbing stairs also
