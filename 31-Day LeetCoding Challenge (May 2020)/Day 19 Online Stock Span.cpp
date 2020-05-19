554462a99ccc8266138a4dfaa2705f1385be1539
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-05-19 18:55:55 +0800

    Create Day 19 Online Stock Span.cpp

---
+++ 31-Day LeetCoding Challenge (May 2020)/Day 19 Online Stock Span.cpp
@@ -0,0 +1,25 @@
+// Explanation : https://leetcode.com/problems/online-stock-span/discuss/640358/JAVA-Solution-With-visualization-and-easy-explained!
+class StockSpanner {
+public:
+	stack<vector<int>> s; 
+    StockSpanner() {        
+    }
+    
+    int next(int price) {
+        int span = 1; // default value
+        while (!s.empty() && price >= s.top()[0]){
+        	span += s.top()[1];
+        	s.pop();
+        }
+        vector<int> temp = {price,span};
+        s.push(temp);
+        return span;
+    }
+};
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
