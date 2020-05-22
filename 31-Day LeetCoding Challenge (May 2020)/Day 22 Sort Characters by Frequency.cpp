9e297963ae3163aa1fe2a9072ddea750682539ce
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-05-22 19:51:45 +0800

    Create Day 22 Sort Characters by Frequency.cpp

---
+++ 31-Day LeetCoding Challenge (May 2020)/Day 22 Sort Characters by Frequency.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> m;
+        string res = "";
+        for (char x : s){
+            m[x]++;
+        }
+        priority_queue<pair<int, char>> pq;
+        for (auto x : m){
+            pq.push({x.second, x.first});
+        }
+        while(!pq.empty()){
+            auto x = pq.top();
+            pq.pop();
+            int t = x.first;
+            while(t--){
+                res += x.second;
+            }
+        }
+        return res;
+    }
+};
+// another solution
+string frequencySort(string s) {
+        int count[256] = {0};
+        for(char c : s)
+            count[c] += 1;
+        vector<pair<int,char>> arr;
+        for(int c = 0; c < 256; ++c)
+        {
+            if(count[c]) arr.push_back({count[c],c});
+        }
+        sort(arr.rbegin(), arr.rend()); // reverse, from big to small
+        string res;
+        for(auto p : arr)
+        {
+            res += string(p.first, p.second);
+        }
+        return res;
+    }
