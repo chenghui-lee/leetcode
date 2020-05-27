c82a8f7a587719b2ea9cc0b194ce2722cf8bf3e4
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-05-27 18:09:36 +0800

    Create Day 27 Possible Bipartition.java

---
+++ 31-Day LeetCoding Challenge (May 2020)/Day 27 Possible Bipartition.java
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/possible-bipartition/discuss/158957/Java-DFS-solution
+class Solution {
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        int[][] graph = new int[N][N];
+        // n by n matrix
+        // row is N-th person, hates N-th column person
+        for (int[] d : dislikes) {
+            graph[d[0] - 1][d[1] - 1] = 1;
+            graph[d[1] - 1][d[0] - 1] = 1;
+        }
+        int[] group = new int[N];
+        for (int i = 0; i < N; i++) {
+            if (group[i] == 0 && !dfs(graph, group, i, 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean dfs(int[][] graph, int[] group, int index, int g) {
+        group[index] = g;
+        for (int i = 0; i < graph.length; i++) {
+            if (graph[index][i] == 1) { // if the person dislike this guy (i)
+                if (group[i] == g) {
+                    return false;
+                }
+                if (group[i] == 0 && !dfs(graph, group, i, -g)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
