471944d9cd8002696549d716f710faddb29e21e8
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-06-30 16:35:23 +0800

    Create Day 30 Word Search II.java

---
+++ 30 Day LeetCoding Challenge(June 2020)/Day 30 Word Search II.java
@@ -0,0 +1,84 @@
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        if(board == null || board.length == 0 || words == null) return res;
+        boolean flag = false;
+        for(String word : words){
+            flag = false;
+            for(int i = 0; i < board.length; i++){
+                for(int j = 0; j < board[0].length; j++){
+                    if(board[i][j] == word.charAt(0)){
+                        boolean[][] visited = new boolean[board.length][board[0].length];
+                        if(dfs(board, i, j, word, visited)){
+                            res.add(word);					
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+                if(flag) break;
+            }
+        }
+        
+        return res;
+    }
+    
+    boolean dfs(char[][] board, int i, int j, String word, boolean[][] visited){
+        if(word.length() == 0) return true;
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(0) || visited[i][j]) return false;
+        visited[i][j] = true;
+        boolean next = dfs(board, i - 1, j, word.substring(1), visited) || dfs(board, i, j - 1, word.substring(1), visited) || dfs(board, i, j + 1, word.substring(1), visited) || dfs(board, i + 1, j, word.substring(1), visited);
+        visited[i][j] = false;
+        return next;
+    }
+    
+}
+
+// another solution in c++, using trie,  @haruhiku
+class Solution {
+public:
+    struct TrieNode {
+        vector<TrieNode*> child;
+        string word;
+        TrieNode() : word(""), child(vector<TrieNode*>(26, nullptr)) {}
+    };
+    
+    TrieNode* buildTrie(vector<string>& words) {
+        TrieNode* root = new TrieNode();
+        for (string w : words) {
+            TrieNode* curr = root;
+            for (char c : w) {
+                int i = c - 'a';
+                if (curr->child[i] == NULL) curr->child[i] = new TrieNode();
+                curr = curr->child[i];
+            }
+            curr->word = w;
+        }
+        return root;
+    }
+
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        vector<string> out;
+        TrieNode* root = buildTrie(words);
+        for(int i = 0; i < board.size(); ++i) 
+            for(int j = 0; j < board[0].size(); ++j) 
+                dfs(board, i, j, root, out);
+        return out;
+    }
+    
+    void dfs(vector<vector<char>>& board, int i, int j, TrieNode* curr, vector<string>& out) {
+        char c = board[i][j];
+        if(c == '#' || curr->child[c - 'a'] == NULL) return;
+        curr = curr->child[c - 'a'];
+        if (curr->word != "") {
+            out.push_back(curr->word);
+            curr->word = "";
+        }
+        board[i][j] = '#';
+        if(i > 0) dfs(board, i - 1, j , curr, out); 
+        if(j > 0) dfs(board, i, j - 1, curr, out);
+        if(i < board.size() - 1) dfs(board, i + 1, j, curr, out); 
+        if(j < board[0].size() - 1) dfs(board, i, j + 1, curr, out); 
+        board[i][j] = c;
+    }
+};
