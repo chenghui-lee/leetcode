54ec4dc0c4db5b4ee14836ee7966d2b67f0bd258
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-07-07 16:45:21 +0800

    Create Day 07 Island Perimeter.cpp

---
+++ July LeetCoding Challenge (2020)/Day 07 Island Perimeter.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int perimeter = 0;
+    int islandPerimeter(vector<vector<int>>& grid) {
+        if(grid.size()==0) return 0;
+        for(int i=0;i<grid.size(); i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j] == 1){
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return perimeter;
+    }
+    
+    void dfs(vector<vector<int>>& grid,int i, int j){
+        if(i<0 || i>= grid.size() || j<0 || j>=grid[0].size() || grid[i][j]==0){
+            perimeter++;
+            return;
+        }
+        else if(grid[i][j] == 2){
+            return;
+        }
+        grid[i][j] = 2;
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+        dfs(grid,i-1,j);
+    }
+};
