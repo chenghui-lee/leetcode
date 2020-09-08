7c643eb74052a7102e29b9bb3adc6c63308eb594
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-09-08 17:15:54 +0800

    Create Day 08 Sum of Root To Leaf Binary Numbers.cpp

---
+++ September LeetCoding Challenge(2020)/Day 08 Sum of Root To Leaf Binary Numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sum = 0;
+    int sumRootToLeaf(TreeNode* root) {
+        dfs(root, 0);
+        return sum;
+    }
+    void dfs(TreeNode* node, int parent){
+        if(!node) return;
+        if(!node->left && !node->right){
+            sum += (parent << 1) + node->val;
+            return;
+         }
+        dfs(node->left, (parent << 1) + node->val);
+        dfs(node->right, (parent << 1) + node->val);
+    }
+};
