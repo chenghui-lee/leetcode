af9f7feeaf1566b75df895ef4b70502a8810afcf
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-09-11 17:19:08 +0800

    Create Day 11 Maximum Product Subarray.cpp

---
+++ September LeetCoding Challenge(2020)/Day 11 Maximum Product Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int prefix = 0, suffix = 0, res = 0;
+        for(int i=0; i<nums.size(); i++){
+            prefix = (prefix ? prefix : 1) * nums[i];
+            suffix = (suffix ? suffix : 1) * nums[i];
+            res = max(res, max(prefix, suffix));
+        }
+        return res;
+    }
+};
