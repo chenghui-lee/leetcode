791813aee5efdf819f798b7aefd2107b22119ffc
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-09-17 17:28:27 +0800

    Create Day 17 Robot Bounded In Circle.cpp

---
+++ September LeetCoding Challenge(2020)/Day 17 Robot Bounded In Circle.cpp
@@ -0,0 +1,17 @@
+// true if robot changed facing direction(not north anymore) OR changed position 
+
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        int x = 0, y = 0, i = 0;
+        vector<vector<int>> direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        for (char & ins : instructions)
+            if (ins == 'R')
+                i = (i + 1) % 4;
+            else if (ins == 'L')
+                i = (i + 3) % 4;
+            else
+                x += direction[i][0], y += direction[i][1];
+        return x == 0 && y == 0 || i > 0;
+    }
+};
