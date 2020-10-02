a6b557b69b3aa1affd62c8a115f6b7e76756da9e
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-10-02 18:24:03 +0800

    Create Day 02 Combination Sum.cpp

---
+++ October LeetCoding Challenge(2020)/Day 02 Combination Sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> res;
+        vector<int> temp;
+        dfs(candidates, target, 0, res, temp);
+        return res;
+    }
+    void dfs(vector<int>& candidates, int target, int start, vector<vector<int>>& res, vector<int>& temp){
+        if (target == 0){
+            res.push_back(temp);
+            return;
+        }
+        for(int i=start; i<candidates.size(); i++){
+            if (candidates[i] > target) break;
+            temp.push_back(candidates[i]);
+            dfs(candidates, target - candidates[i], i, res, temp);
+            temp.pop_back();
+        }
+    }
+};
