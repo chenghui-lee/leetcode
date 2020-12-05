d8505e18c4d7253f11aebae52c5efc5daec14ec0
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-12-05 16:52:55 +0800

    Create Day 05 Can Plant Flowers.cpp

---
+++ December LeetCoding Challenge(2020)/Day 05 Can Plant Flowers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0;
+        for(int i=0; i<flowerbed.size(); i++){
+            if (flowerbed[i] == 0 && (i == 0||flowerbed[i-1] == 0) && (i == flowerbed.size() -1 || flowerbed[i+1] == 0)) {
+                count++;
+                flowerbed[i] = 1;
+            }
+        }
+        return count >= n;
+    }
+};
