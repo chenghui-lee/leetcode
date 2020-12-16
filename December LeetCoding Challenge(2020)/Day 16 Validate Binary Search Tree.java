36c2be22569fa3c473bd2ec3b2cdf2491ca2e916
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-12-16 16:54:52 +0800

    Create Day 16 Validate Binary Search Tree.java

---
+++ December LeetCoding Challenge(2020)/Day 16 Validate Binary Search Tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode prev = null;
+        
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if(prev != null && root.val <= prev.val) return false;
+            prev = root;
+            root = root.right;
+        }
+        return true;
+    }
+}
