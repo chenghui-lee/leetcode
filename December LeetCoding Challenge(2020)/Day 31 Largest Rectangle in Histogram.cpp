4aad38c307357761449767bb3704f213debe056b
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-12-31 18:05:42 +0800

    Create Day 31 Largest Rectangle in Histogram.cpp

---
+++ December LeetCoding Challenge(2020)/Day 31 Largest Rectangle in Histogram.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        if (heights.size() == 0) return 0;
+        int n = heights.size();
+        vector<int> left(n, 0); // leftmost element that is larger than i-th buiding
+        vector<int> right(n, 0); // rightmost element that is larger than i-th building
+
+        left[0] = -1;
+        right[n-1] = n;
+        
+        for(int i=1; i<n; i++){
+            int idx = i-1;
+            while(idx >=0 && heights[idx] >= heights[i]){
+                idx = left[idx];
+            }
+            left[i] = idx;
+        }
+        
+        for (int i = n - 2; i >= 0; i--) {
+            int idx = i + 1;
+
+            while (idx < n && heights[idx] >= heights[i]) {
+                idx = right[idx];
+            }
+            right[i] = idx;
+        }
+        int res = 0;
+        for(int i=0; i<n; i++){
+            res = max(res, heights[i] * (right[i] - left[i] - 1));
+        }
+        return res;
+    }
+};
