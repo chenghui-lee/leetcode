13b2c94f643b82d04e7028e7dab6dfdf50ffaa06
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-01-02 17:01:41 +0800

    Update Day 02 Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp

--- January LeetCoding Challenge(2021)/Day 02 Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
+++ January LeetCoding Challenge(2021)/Day 02 Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
@@ -24,3 +24,22 @@ public:
         return dfs(cloned, target->val);
     }
 };
+
+// Comparing references instead of values
+
+class Solution {
+public:
+    TreeNode* dfs(TreeNode* ori, TreeNode* copy, TreeNode* target){
+        if (!ori) return NULL;
+        
+        if (ori == target) return copy;
+        TreeNode* left = dfs(ori->left, copy->left, target);
+        TreeNode* right = dfs(ori->right, copy->right, target);
+        if (left) return left;
+        else if (right) return right;
+        else return NULL;
+    }
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        return dfs(original, cloned, target);
+    }
+};
