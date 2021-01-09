a1f40e001ae1ad51713e11fb09d6ef09217c7dc4
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-01-09 11:25:50 +0800

    Create 0354 Russian Doll Envelopes.cpp

---
+++ Code/0354 Russian Doll Envelopes.cpp
@@ -0,0 +1,26 @@
+/* 
+Decrease to 1D by sorting the width in ACS, if got tie, sort height in DESC
+This is to prevent [3,3] to fit inside [3,4]
+The rest is just longest increasing subsequence (LIS)
+*/
+
+class Solution {
+private:
+    static bool comp(vector<int>& a, vector<int>& b){
+        if (a[0] == b[0]) return b[1] < a[1]; // desc in height
+        else return a[0] < b[0]; // asc in width
+    }
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        if (envelopes.size() == 0) return 0;
+        sort(envelopes.begin(), envelopes.end(), comp);
+        
+        vector<int> dp;
+        for(auto pair : envelopes){
+            auto it = lower_bound(dp.begin(), dp.end(), pair[1]);
+            if (it == dp.end()) dp.push_back(pair[1]);
+            else *it = pair[1];
+        }
+        return dp.size();
+    }
+};
