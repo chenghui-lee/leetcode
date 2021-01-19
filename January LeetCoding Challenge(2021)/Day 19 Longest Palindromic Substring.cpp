caa2bfc2f81936cd1e3c11d8f086a083b27f70f3
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-01-19 18:16:18 +0800

    Create Day 19 Longest Palindromic Substring.cpp

---
+++ January LeetCoding Challenge(2021)/Day 19 Longest Palindromic Substring.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxLen = 0, low = 0;
+    void expand(string s, int i, int j){
+        while(i>=0 && j<s.size() && s[i] == s[j]){
+            i--;
+            j++;
+        }
+        if (j - i - 1 > maxLen){
+            maxLen = j - i - 1;
+            low = i+1;
+        }
+    }
+    
+    string longestPalindrome(string s) {
+        
+        for(int i=0; i<s.size(); i++){
+            expand(s, i, i);
+            expand(s, i, i+1);
+        }
+        return s.substr(low, maxLen);
+    }
+};
