9f1a7d952cfcc02a481493676a2220ddc53858a0
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-01-20 20:16:23 +0800

    Create Day 20 Valid Parentheses.cpp

---
+++ January LeetCoding Challenge(2021)/Day 20 Valid Parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        
+        for(int i=0; i<s.size(); i++){
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
+            else if (st.size() && s[i] == ')' && st.top() == '('){
+                st.pop();
+            }else if (st.size() && s[i] == '}' && st.top() == '{'){
+                st.pop();
+            }else if (st.size() && s[i] == ']' && st.top() == '['){
+                st.pop();
+            }else return false;
+        }
+        return st.empty();
+    }
+};
