d9809e893bc93b0d7ad0c7e3b6874f543cf07c75
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-02-14 16:52:50 +0800

    Update Day 14 Is Graph Bipartite?.cpp

--- February LeetCoding Challenge(2021)/Day 14 Is Graph Bipartite?.cpp
+++ February LeetCoding Challenge(2021)/Day 14 Is Graph Bipartite?.cpp
@@ -20,3 +20,32 @@ public:
         return true;
     }
 };
+
+
+// Same concept, but BFS
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> grp(n);
+        for(int i=0; i<n; i++){
+            if (grp[i] != 0) continue;
+            
+            queue<int> q;
+            q.push(i);
+            grp[i] = 1;
+            while(q.size()){
+                int cur = q.front();
+                q.pop();
+                for(auto& next : graph[cur]){
+                    if (grp[next] == 0){
+                        grp[next] = -grp[cur];
+                        q.push(next);
+                    }
+                    else if (grp[cur] == grp[next]) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
