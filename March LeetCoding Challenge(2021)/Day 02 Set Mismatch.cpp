a88357346b91f57ff45de3cae57b437bec873f35
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-03-02 17:08:03 +0800

    Update Day 02 Set Mismatch.cpp

--- March LeetCoding Challenge(2021)/Day 02 Set Mismatch.cpp
+++ March LeetCoding Challenge(2021)/Day 02 Set Mismatch.cpp
@@ -16,3 +16,16 @@ public:
         return res;
     }
 };
+
+// O(1) Space, O(n) time
+vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> res(2);
+        for(auto x : nums){
+            if (nums[abs(x)-1] < 0) res[0] = abs(x);
+            else nums[abs(x)-1] *= -1;
+        }
+        for (int i=0;i<nums.size();i++) {
+            if (nums[i] > 0) res[1] = i+1;
+        }
+        return res;
+}
