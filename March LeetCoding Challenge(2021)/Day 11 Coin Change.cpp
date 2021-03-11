8936f0dd01336f7328445a83d05c95e1eba3cb74
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-03-11 18:10:26 +0800

    Create Day 11 Coin Change.cpp

---
+++ March LeetCoding Challenge(2021)/Day 11 Coin Change.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> dp(amount+1, amount+1); // min number of coins to get amount i
+        dp[0] = 0;
+        
+        for(int i=1; i<=amount; i++){
+            for(int j=0; j<n; j++){
+                if (i - coins[j] >= 0){
+                    dp[i] = min(dp[i], dp[i-coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount? -1 : dp[amount];
+    }
+};
