50520e85ad110c01b61d7322cfa34b9965deefe1
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-03-12 16:31:49 +0800

    Create Day 12 Check If a String Contains All Binary Codes of Size K.cpp

---
+++ March LeetCoding Challenge(2021)/Day 12 Check If a String Contains All Binary Codes of Size K.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        int n = s.size();
+        unordered_set<int> st;
+        for(int i=0; i+k-1<n; i++){
+            st.insert(stoi(s.substr(i, k), 0, 2));
+        }
+        for(int i=0 ;i < (1 << k); i++){
+            if (!st.count(i)) return false;
+        }
+        return true;
+    }
+};
