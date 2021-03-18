85ad1bd50937e5b616ea6d87923c5f1a56b97c26
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-03-18 17:41:49 +0800

    Create Day 18 Wiggle Subsequence.cpp

---
+++ March LeetCoding Challenge(2021)/Day 18 Wiggle Subsequence.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/wiggle-subsequence/discuss/84849/Very-Simple-Java-Solution-with-detail-explanation
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        if (n<2) return n;
+        
+        int idx = 1;
+        while (idx < n && nums[idx-1] == nums[idx]){
+            idx++;
+        }
+        if (idx == n) return 1; // all the same numbers
+        
+        bool increasing = nums[idx] > nums[0]; // what we expecting now
+        int prev = nums[0], res = 1;
+        for(int i=idx; i<n; i++){
+            if ((increasing && nums[i] > prev) || (!increasing && nums[i] < prev)){
+                increasing = !increasing;
+                res++;
+            }
+            prev = nums[i];
+        }
+        return res;
+    }
+};
