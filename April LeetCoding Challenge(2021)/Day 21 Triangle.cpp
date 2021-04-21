1f967760ebad612e3718661e05c5fad1e0ca88ea
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-04-21 17:07:16 +0800

    Create Day 21 Triangle.cpp

---
+++ April LeetCoding Challenge(2021)/Day 21 Triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<int> dp;
+        dp.push_back(triangle[0][0]);
+        
+        for(int i=1; i<triangle.size(); i++){
+            int n = triangle[i].size();
+            vector<int> temp(n);
+            for(int j=0; j<n; j++){
+                if (j == 0){
+                    temp[j] = dp[j] + triangle[i][j];
+                }else if (j == n-1){
+                    temp[j] = dp[j-1] + triangle[i][j];
+                }else{
+                    temp[j] = min(dp[j], dp[j-1]) + triangle[i][j];
+                }
+            }
+            dp = temp;
+        }
+        
+        return *min_element(dp.begin(), dp.end());
+    }
+};
