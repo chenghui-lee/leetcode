767be967775d2ca072c40ec04d25ba473b5496e1
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-06-02 17:57:55 +0800

    Create Day 02 Interleaving String.cpp

---
+++ June LeetCoding Challenge(2021)/Day 02 Interleaving String.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.size() + s2.size() != s3.size()) return false;
+        
+        int m = s1.size(), n = s2.size();
+        
+        vector<vector<bool>> table(m+1, vector<bool>(n+1)); // interleaving on pos i j or not
+        
+        for(int i=0; i<=m; i++){
+            for(int j=0; j<=n; j++){
+                if(i==0 && j==0)
+                    table[i][j] = true;
+                else if(i == 0)
+                    table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
+                else if(j == 0)
+                    table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
+                else
+                    table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
+            }
+        }
+        return table[m][n];
+    }
+};
