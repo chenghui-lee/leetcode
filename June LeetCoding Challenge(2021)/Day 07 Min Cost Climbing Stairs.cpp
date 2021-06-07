e4776c24ae88ffaf3ce33c9365d28e2cd5d0d1a2
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-06-07 17:04:29 +0800

    Create Day 07 Min Cost Climbing Stairs.cpp

---
+++ June LeetCoding Challenge(2021)/Day 07 Min Cost Climbing Stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n+1); // cost to reach top from step i
+        
+        dp[n-1] = cost[n-1];
+        
+        for(int i=n-2; i>=0; i--){
+            dp[i] = cost[i] + min(dp[i+1], dp[i+2]);   
+        }
+        return min(dp[0], dp[1]);
+    }
+};
