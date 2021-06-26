582038b2f7ec32dd14febe50c5e1c623a5c78519
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-06-26 19:05:36 +0800

    Create Day 26 Count of Smaller Numbers After Self.cpp

---
+++ June LeetCoding Challenge(2021)/Day 26 Count of Smaller Numbers After Self.cpp
@@ -0,0 +1,45 @@
+// Binary index tree
+// FenwickTree
+class FenwickTree {    
+public:
+    FenwickTree(int n): sums_(n + 1, 0) {}
+    
+    void update(int i, int delta) {
+        while (i < sums_.size()) {
+            sums_[i] += delta;
+            i += LSOne(i);
+        }
+    }
+    
+    int query(int i) const {        
+        int sum = 0;
+        while (i > 0) {
+            sum += sums_[i];
+            i -= LSOne(i);
+        }
+        return sum;
+    }
+private:
+    static inline int LSOne(int x) { return x & (-x); }
+    vector<int> sums_;
+};
+
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        set<int> s(nums.begin(), nums.end());
+        int temp = 0;
+        unordered_map<int, int> rank; // num, rank 1..k, 1 = smallest element
+        for(auto x : s){
+            rank[x] = ++temp;
+        }
+        
+        FenwickTree tree(s.size());
+        vector<int> res(nums.size());
+        for(int i=nums.size()-1; i>=0; i--){
+            res[i] = tree.query(rank[nums[i]]-1);
+            tree.update(rank[nums[i]], 1);
+        }
+        return res;
+    }
+};
