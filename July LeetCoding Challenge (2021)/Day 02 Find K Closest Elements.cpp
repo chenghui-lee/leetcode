2072494315a4287ab40f2876ce54d37b21b61aca
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-07-02 16:59:53 +0800

    Create Day 02 Find K Closest Elements.cpp

---
+++ July LeetCoding Challenge (2021)/Day 02 Find K Closest Elements.cpp
@@ -0,0 +1,44 @@
+// using lower bound
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        auto it = lower_bound(arr.begin(), arr.end(), x);
+        int idx = distance(arr.begin(), it);
+        
+        int left = idx-1, right = idx;
+        vector<int> res;
+        
+        while(k > 0){
+            if (left >=0 && right < n){
+                if (x - arr[left] <= arr[right] - x) res.push_back(arr[left--]);
+                else res.push_back(arr[right++]);
+            }else if(left >= 0) res.push_back(arr[left--]);
+            else res.push_back(arr[right++]);
+            k--;
+        }
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
+
+// using binary search
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0, right = arr.size() - k; // note here is k, not 1
+        
+        // use binary search to find the rightmost starting index
+        while(left < right){
+            int mid = left + (right -left)/2;
+            
+            if (x - arr[mid] > arr[mid + k] - x){
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return vector<int>(arr.begin()+left, arr.begin()+left+k);
+        
+    }
+};
