3fc287980707018b394b16bbc12a8c9234f4e32f
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-07-21 16:55:20 +0800

    Create Day 21 Push Dominoes.cpp

---
+++ July LeetCoding Challenge (2021)/Day 21 Push Dominoes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        int n = dominoes.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        
+        // right
+        int force = 0;
+        for(int i=0; i<n; i++){
+            if (dominoes[i] == 'R') force = n;
+            else if (dominoes[i] == 'L') force = 0;
+            else force = max(force-1, 0);
+            right[i] = force;
+        }
+        // left
+        force = 0;
+        for(int i=n-1; i>=0; i--){
+            if (dominoes[i] == 'L') force = n;
+            else if (dominoes[i] == 'R') force = 0;
+            else force = max(force-1, 0);
+            left[i] = force;
+        }
+        for(int i=0; i<n; i++){
+            if (left[i] > right[i]) dominoes[i] = 'L';
+            else if (right[i] > left[i]) dominoes[i] = 'R';
+            else dominoes[i] = '.';
+        }
+        return dominoes;
+    }
+};
