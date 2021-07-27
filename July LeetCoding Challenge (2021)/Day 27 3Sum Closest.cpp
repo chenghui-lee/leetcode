a2dec0bfd743ff82e59ac373a931564626914584
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-07-27 16:14:06 +0800

    Create Day 27 3Sum Closest.cpp

---
+++ July LeetCoding Challenge (2021)/Day 27 3Sum Closest.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        
+        int curMin = INT_MAX, n = nums.size(), res = 0;
+        
+        for(int i=0; i<nums.size()-2; i++){
+            if(i==0 || nums[i] != nums[i-1]){
+                int j = i+1, k = n-1;
+                int bal = target - nums[i];
+                while(j < k){
+                    if (curMin > abs(nums[i] + nums[j] + nums[k] - target)){
+                        res = nums[i] + nums[j] + nums[k];
+                        curMin = abs(nums[i] + nums[j] + nums[k] - target);
+                    }
+                    if (nums[j] + nums[k] > bal) k--;
+                    else if (nums[j] + nums[k] < bal) j++;
+                    else return target;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
