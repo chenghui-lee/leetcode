395436fea8c671f5c62510a1d81aa85b1ac98050
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-08-16 15:47:35 +0800

    Create Day 16 Range Sum Query - Immutable.cpp

---
+++ August LeetCoding Challenge (2021)/Day 16 Range Sum Query - Immutable.cpp
@@ -0,0 +1,20 @@
+class NumArray {
+public:
+    vector<int> prefix;
+    NumArray(vector<int>& nums) {
+        prefix = vector<int>(nums.size() + 1);
+        for(int i=0; i<nums.size(); i++){
+            prefix[i+1] = prefix[i] + nums[i];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return prefix[right+1] - prefix[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
