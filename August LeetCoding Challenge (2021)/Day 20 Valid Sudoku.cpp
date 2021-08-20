094521fae62d4b5109294d47fc84fc4db79540d2
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-08-20 16:34:54 +0800

    Create Day 20 Valid Sudoku.cpp

---
+++ August LeetCoding Challenge (2021)/Day 20 Valid Sudoku.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<bool> num;
+        // check each col
+        for(int i=0; i<9; i++){
+            num = vector<bool>(10, false);
+            for(int j=0; j<9; j++){
+                if (board[i][j] == '.') continue;
+                auto idx = board[i][j] - '0';
+                if (num[idx]) return false;
+                num[idx] = true;
+            }
+        }
+        // check each row
+        for(int i=0; i<9; i++){
+            num = vector<bool>(10, false);
+            for(int j=0; j<9; j++){
+                if (board[j][i] == '.') continue;
+                auto idx = board[j][i] - '0';
+                if (num[idx]) return false;
+                num[idx] = true;
+            }
+        }
+        // check whole box
+
+        int rowStart = 0, colStart = 0;
+        while(rowStart < 9){
+            num = vector<bool>(10, false);
+            for(int i=rowStart; i<rowStart+3; i++){
+                for(int j=colStart; j<colStart+3; j++){
+                    if (board[i][j] == '.') continue;
+                    auto idx = board[i][j] - '0';
+                    if (num[idx]) return false;
+                    num[idx] = true;
+                }
+            }
+            if (colStart < 9) colStart+=3;
+            if (colStart == 9){
+                colStart = 0, rowStart+=3;
+            }
+        }
+        return true;
+        
+    }
+};
