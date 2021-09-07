fab48fb054ce2b95c550db4e1803d9e63439a671
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-09-07 15:20:04 +0800

    Create Day 07 Reverse Linked List.cpp

---
+++ September LeetCoding Challenge (2021)/Day 07 Reverse Linked List.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* prev = NULL;
+        
+        while(cur){
+            auto nxt = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = nxt;
+        }
+        return prev;
+    }
+};
+
+// recursion
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return newHead;
+        
+    }
+};
