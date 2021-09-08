8c710b6ce567e20f5f955a76d54addff934c8fc2
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-09-08 17:05:03 +0800

    Update 0121 Best Time to Buy and Sell Stock.cpp

--- Dynamic Programming I/0121 Best Time to Buy and Sell Stock.cpp
+++ Dynamic Programming I/0121 Best Time to Buy and Sell Stock.cpp
@@ -14,3 +14,17 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int res = 0, hold = INT_MAX;
+        
+        for(int i=0; i<n; i++){
+            hold = min(hold, prices[i]);
+            res = max(res, prices[i] - hold);
+        }
+        return res;
+    }
+};
