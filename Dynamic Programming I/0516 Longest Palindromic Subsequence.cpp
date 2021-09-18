53fa88acef745a1b1c8986f44097c9ac6789c9e5
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-09-18 13:07:38 +0800

    Create 0516 Longest Palindromic Subsequence.cpp

---
+++ Dynamic Programming I/0516 Longest Palindromic Subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        for(int i=0; i<n; i++) dp[i][i] = 1;
+        
+        for(int len=2; len<=n; len++){
+            for(int i=0; i+len-1<n; i++){
+                int j = i+len-1;
+                if (s[i] == s[j]){
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                }else{
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+};
