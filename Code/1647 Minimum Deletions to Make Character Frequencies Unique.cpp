d0ba2e88170d2f348773187c3f73c7353c2efba5
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-09-21 10:32:23 +0800

    Update 1647 Minimum Deletions to Make Character Frequencies Unique.cpp

--- Code/1647 Minimum Deletions to Make Character Frequencies Unique.cpp
+++ Code/1647 Minimum Deletions to Make Character Frequencies Unique.cpp
@@ -23,3 +23,25 @@ public:
         return res;
     }
 };
+// another way
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int> count(26, 0);
+        for(auto& ch : s){
+            count[ch - 'a']++;
+        }
+        sort(begin(count), end(count));
+        
+        int res = 0;
+        int large = count[25];
+        for(int i=25; i>=0; i--){
+            if (count[i] > large){
+                res += count[i] - large;
+                count[i] = large;
+            }
+            large = max(count[i] - 1, 0);
+        }
+        return res;
+    }
+};
