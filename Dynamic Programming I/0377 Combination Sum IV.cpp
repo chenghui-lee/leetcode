f3a7173745ced05eb1d826cae5e5ab29f5ca7c93
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-09-22 09:12:46 +0800

    Create 0377 Combination Sum IV.cpp

---
+++ Dynamic Programming I/0377 Combination Sum IV.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned long long> dp(target + 1, 0);
+        dp[0] = 1;
+        
+        for(int i=1; i<=target; i++){
+            for(int j=0; j<nums.size(); j++){
+                if (nums[j] <= i){
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
