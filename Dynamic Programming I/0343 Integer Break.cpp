dfdc98b060de682206e2b650f19d1781e8904163
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-09-22 09:50:55 +0800

    Update 0343 Integer Break.cpp

--- Dynamic Programming I/0343 Integer Break.cpp
+++ Dynamic Programming I/0343 Integer Break.cpp
@@ -14,3 +14,22 @@ public:
         return dp[n];
     }
 };
+
+// another solution using maths, o(n)
+// https://leetcode.com/problems/integer-break/discuss/80689/A-simple-explanation-of-the-math-part-and-a-O(n)-solution
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+        int product = 1;
+        
+        while(n>4){
+            product*=3;
+            n-=3;
+        }
+        product*=n;
+        
+        return product;
+    }
+};
