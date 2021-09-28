e131c2c2038d20976f44fd0e5eb6531df07c7a2c
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-09-28 12:09:59 +0800

    Create 2017 Grid Game.cpp

---
+++ Code/2017 Grid Game.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/grid-game/discuss/1486340/C%2B%2BJavaPython-Robot1-Minimize-TopSum-and-BottomSum-of-Robot-2-Picture-Explained
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        long long topsum = 0, bottomsum = 0, res = LLONG_MAX;
+        topsum = accumulate(grid[0].begin(), grid[0].end(), 0LL);
+        
+        for(int i=0; i<grid[0].size(); i++){
+            topsum -= grid[0][i];
+            res = min(res, max(topsum, bottomsum));
+            bottomsum += grid[1][i];
+        }
+        return res;
+    }
+};
