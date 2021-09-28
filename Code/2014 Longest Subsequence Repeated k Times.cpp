e429d813cff39afdc551cfcd715ef4da66c00007
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-09-28 11:03:44 +0800

    Create 2014 Longest Subsequence Repeated k Times.cpp

---
+++ Code/2014 Longest Subsequence Repeated k Times.cpp
@@ -0,0 +1,53 @@
+// 232ms, 70.99%
+// 66.2MB, 18.7%
+/*
+basically use bfs to generate all possible subsequence, start from the shortest to longest one
+in bfs, will filter out those invalid one
+*/
+class Solution {
+public:
+    // check if the cur substring repeated k times in s
+    bool isSub(string s, string cur, int k){
+        int j = 0, count = 0;
+        for(int i=0; i<s.size(); i++){
+            if (s[i] == cur[j]){
+                j++;
+                if (j == cur.size()){
+                    count++;
+                    j = 0;
+                }
+                if (count == k) return true;
+            }
+        }
+        return false;
+    }
+    
+    string longestSubsequenceRepeatedK(string s, int k) {
+        queue<string> q;
+        map<char, int> count;
+        for(int i=0; i<s.size(); i++){
+            count[s[i]]++;
+        }
+        // optimization, we only use those char with frequency >= k
+        vector<char> possibleChar;
+        for(auto p : count){
+            if (p.second >= k) possibleChar.push_back(p.first);
+        }
+        q.push("");
+        string res;
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                string cur = q.front(); q.pop();
+                for(char ch : possibleChar){
+                    string next = cur + ch;
+                    if (isSub(s, next, k)){
+                        res = next;
+                        q.push(next);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
