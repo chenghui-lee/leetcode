6d6721630495fb6e2f54c7a5a53a1d679f8b508e
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-09-30 16:38:34 +0800

    Update Day 30 Partition to K Equal Sum Subsets.cpp

--- September LeetCoding Challenge (2021)/Day 30 Partition to K Equal Sum Subsets.cpp
+++ September LeetCoding Challenge (2021)/Day 30 Partition to K Equal Sum Subsets.cpp
@@ -29,3 +29,34 @@ public:
 };
 
 // sol 2. loop through all numbers, put into any subset
+
+class Solution {
+public:
+    
+    bool canSum(vector<int>& nums, vector<int>& kSum, int target, int index){
+        if (index == nums.size()){
+            for(auto x : kSum){
+                if (x != target) return false;
+            }
+            return true;
+        }
+        for(auto& x : kSum){
+            if (x + nums[index] <= target){
+                x += nums[index];
+                if (canSum(nums, kSum, target, index+1)) return true;
+                x -= nums[index];
+            }
+        }
+        return false;
+    }
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int n = nums.size(), sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % k != 0) return false;
+        
+        int target = sum / k;
+        vector<bool> visited(n, false);
+        vector<int> kSum(k, 0);
+        sort(nums.begin(), nums.end(), greater<int>());
+        return canSum(nums, kSum, target, 0);
+    }
+};
