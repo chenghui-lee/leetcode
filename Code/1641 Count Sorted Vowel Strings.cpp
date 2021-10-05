e2b1811481167a3097135251fd043a26adc03397
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-10-05 09:58:22 +0800

    Create 1641 Count Sorted Vowel Strings.cpp

---
+++ Code/1641 Count Sorted Vowel Strings.cpp
@@ -0,0 +1,54 @@
+// top down
+// O(nk) time and space
+// n = length of string, k = at most k type character
+class Solution {
+public:
+    map<pair<int, int>, int> seen;
+    int help(int n, int k){
+        if (n == 1 || k == 1){
+            return k;
+        }
+        if (seen.find({n, k}) != seen.end()){
+            return seen[{n,k}];
+        }
+        int sum = 0;
+        for(int i=1; i<=k; i++){
+            sum += help(n-1, i);
+        }
+        return seen[{n, k}] = sum;
+    }
+    
+    int countVowelStrings(int n) {
+        return help(n, 5);
+    }
+};
+
+// bottom up
+// O(nk) time and space
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        vector<vector<int>> dp(n+1, vector<int>(6, 0));
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=5; j++){
+                dp[i][j] = dp[i][j-1] + (i > 1? dp[i-1][j] : 1);
+            }
+        }
+        return dp[n][5];
+    }
+};
+
+// bottom up
+// O(nk) time, O(k) space
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        vector<int> dp = {0, 1, 1, 1, 1, 1};
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=5; j++){
+                dp[j] += dp[j-1];
+            }
+        }
+        return dp[5];
+    }
+};
