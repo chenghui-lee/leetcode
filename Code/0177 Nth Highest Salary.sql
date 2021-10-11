0c3c7a2eca5082c76230ada46c5cc13897e79246
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-10-11 16:13:03 +0800

    Create 0177 Nth Highest Salary.sql

---
+++ Code/0177 Nth Highest Salary.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT IFNULL(
+            (SELECT DISTINCT Salary FROM Employee
+            ORDER BY Salary DESC
+            LIMIT 1 OFFSET M), NULL
+      )
+  );
+END
