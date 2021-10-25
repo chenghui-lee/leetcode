e1f8e149a3e2c558a1e802865c23619ec091a993
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-10-25 09:30:44 +0800

    Create Day 25 Min Stack.cpp

---
+++ October LeetCoding Challenge(2021)/Day 25 Min Stack.cpp
@@ -0,0 +1,45 @@
+class MinStack {
+public:
+    vector <int> stack;
+    vector <int> min;
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        if(min.empty() || x <= min.back()){
+            min.push_back(x);
+        }
+        stack.push_back(x);
+    }
+    
+    void pop() {
+        if (stack.back() == min.back()){
+            min.pop_back();
+        }
+        stack.pop_back();
+    }
+    
+    int top() {
+        if (stack.empty()){
+            return -1;
+        }
+        return stack.back();
+    }
+    
+    int getMin() {
+        if (min.empty()){
+            return -1;
+        }
+        return min.back();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
