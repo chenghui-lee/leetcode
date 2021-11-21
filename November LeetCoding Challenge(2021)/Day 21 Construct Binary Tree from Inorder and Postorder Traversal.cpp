f2915d7eaa6436aff8624c23e35f780531a4c67b
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2021-11-21 10:20:58 +0800

    Create Day 21 Construct Binary Tree from Inorder and Postorder Traversal.cpp

---
+++ November LeetCoding Challenge(2021)/Day 21 Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> m; // val, index
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if (inorder.size() == 0 || postorder.size() == 0 || inorder.size() != postorder.size()) return NULL;
+        for(int i=0; i<inorder.size(); i++){
+            m[inorder[i]] = i;
+        }
+        return helper(inorder, postorder, 0, 0, inorder.size());
+    }
+    TreeNode* helper(vector<int>& inorder, vector<int>& postorder, int in_start, int po_start, int size){
+        if(size==0) return NULL;
+        TreeNode* root = new TreeNode(postorder[po_start + size - 1]);
+        // root->val = (); // last element
+        int index = m[root->val];
+        int leftSize = index - in_start;
+        int rightSize = size - leftSize - 1;
+        root->left = helper(inorder, postorder, in_start, po_start, leftSize);
+        root->right = helper(inorder, postorder, index+1, po_start+leftSize, rightSize);
+        return root;
+    }
+};
