d617f363bd97f3221ac92da97d01350dfa9d935b
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-01-17 08:47:29 +0800

    Create Day 17 Word Pattern.java

---
+++ January LeetCoding Challenge(2022)/Day 17 Word Pattern.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        HashMap <Character, String> map_char = new HashMap();
+        HashMap <String, Character> map_word = new HashMap();
+        String[] words = str.split(" ");
+
+        if (words.length != pattern.length()) return false;
+
+        for (int i = 0; i < words.length; i++) {
+            char c = pattern.charAt(i);
+            String w = words[i];
+            if (!map_char.containsKey(c)) {
+                if (map_word.containsKey(w)) {
+                    return false;
+                } else {
+                    map_char.put(c, w);
+                    map_word.put(w, c);
+                }
+
+            } else {
+                String mapped_word = map_char.get(c);
+                if (!mapped_word.equals(w))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
