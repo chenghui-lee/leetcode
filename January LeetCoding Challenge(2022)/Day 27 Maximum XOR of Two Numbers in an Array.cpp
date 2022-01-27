f788a5a9d4c604b9c1d927d70081e2bb31479214
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-01-27 13:35:44 +0800

    Update Day 27 Maximum XOR of Two Numbers in an Array.cpp

--- January LeetCoding Challenge(2022)/Day 27 Maximum XOR of Two Numbers in an Array.cpp
+++ January LeetCoding Challenge(2022)/Day 27 Maximum XOR of Two Numbers in an Array.cpp
@@ -23,3 +23,47 @@ public:
         return res;
     }
 };
+
+// using Trie
+class Solution {
+private:
+    struct Trie{
+        vector<Trie*> children;
+        Trie(): children(vector<Trie*>(2, nullptr)){};
+    };
+    
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        if (nums.size() <= 1) return 0;
+        
+        Trie* root = new Trie();
+        for(int x : nums){
+            Trie* cur = root;
+            for(int i=31; i>=0; i--){
+                int curBit = (x >> i) & 1;
+                if (cur->children[curBit] == nullptr){
+                    cur->children[curBit] = new Trie();
+                }
+                cur = cur->children[curBit];
+            }
+        }
+        
+        int res = INT_MIN;
+        for(int x : nums){
+            Trie* cur = root;
+            int curSum = 0;
+            for(int i=31; i>=0; i--){
+                int curBit = (x >> i) & 1;
+                if (cur->children[curBit ^ 1] != nullptr){
+                    curSum += (1 << i);
+                    cur = cur->children[curBit ^ 1];
+                }else{
+                    cur = cur->children[curBit];
+                }
+            }
+            res = max(res, curSum);
+        }
+        
+        return res;
+    }
+};
