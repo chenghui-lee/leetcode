183c0b2dc887751d3024e2bd893080a4b74fe9e3
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-02-03 09:49:53 +0800

    Create Day 03 4Sum II.cpp

---
+++ February LeetCoding Challenge(2022)/Day 03 4Sum II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        int res = 0;
+        unordered_map<int, int> AB;
+        for (int a : A)
+            for (int b : B)
+                AB[a + b]++;
+        for (int c : C)
+            for (int d : D)
+                res += AB[-c - d];
+        return res;
+    }
+};
