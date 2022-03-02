34defa58dee1ff2bbf186af23694f9bb26e8130c
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-03-02 23:31:09 +0800

    Update Day 02 Is Subsequence.cpp

--- March LeetCoding Challenge(2022)/Day 02 Is Subsequence.cpp
+++ March LeetCoding Challenge(2022)/Day 02 Is Subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int i =0, j = 0;
-        while(j < t.size()){
+        while(i < s.size() && j < t.size()){
             if (s[i] == t[j]) i++;
             j++;
         }
