42aaf77be55ff589b6d94740d4afe4dd8b769f20
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-03-09 09:59:59 +0800

    Update Day 09 Remove Duplicates from Sorted List II.cpp

--- March LeetCoding Challenge(2022)/Day 09 Remove Duplicates from Sorted List II.cpp
+++ March LeetCoding Challenge(2022)/Day 09 Remove Duplicates from Sorted List II.cpp
@@ -34,3 +34,29 @@ public:
         
     }
 };
+
+// using set
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        unordered_set<int> s;
+        ListNode* cur = head;
+        ListNode* prev = NULL;
+        
+        while(cur){
+            if (cur->next && cur->val ==  cur->next->val || s.find(cur->val) != s.end()){
+                s.insert(cur->val);
+                if (prev == NULL){
+                    head = cur->next;
+                }else{
+                    prev->next = cur->next;
+                }
+                cur = cur->next;
+            }else{
+                prev = cur;
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
