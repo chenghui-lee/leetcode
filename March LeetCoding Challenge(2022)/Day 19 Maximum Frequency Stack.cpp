82eb0687bfd9987349079c79dc4bd89d4318bc59
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-03-19 20:12:02 +0800

    Create Day 19 Maximum Frequency Stack.cpp

---
+++ March LeetCoding Challenge(2022)/Day 19 Maximum Frequency Stack.cpp
@@ -0,0 +1,34 @@
+class FreqStack {
+public:
+    vector<vector<int>> num;
+    unordered_map<int, int> m; // num, frequency
+    int maxOcc = 0;
+    FreqStack() {
+        num = vector<vector<int>>(10001);
+    }
+    
+    void push(int x) {
+        m[x]++;
+        num[m[x]].push_back(x);
+        maxOcc = max(maxOcc, m[x]);
+    }
+    
+    int pop() {
+        for(int i=maxOcc; i>=1; i--){
+            if (num[i].size()){
+                int ans = num[i].back();
+                m[ans]--;
+                num[i].pop_back();
+                return ans;
+            }
+        }
+        return -1; // not possible
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ */
