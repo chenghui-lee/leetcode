a3a740d8b0f3113cead07cab31bbb57e645da5ae
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-03-23 13:15:27 +0800

    Create Day 23 Broken Calculator.cpp

---
+++ March LeetCoding Challenge(2022)/Day 23 Broken Calculator.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int count = 0;
+        
+        while(target > startValue){
+            if (target % 2 == 0) target /= 2;
+            else target++;
+            count++;
+        }
+        int diff = startValue - target;
+        return count + diff;
+        
+    }
+};
