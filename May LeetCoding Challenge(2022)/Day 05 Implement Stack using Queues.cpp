f3d07be6fe5e22b48f99689cbafd92cac38a57f3
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-05-05 08:56:22 +0800

    Create Day 05 Implement Stack using Queues.cpp

---
+++ May LeetCoding Challenge(2022)/Day 05 Implement Stack using Queues.cpp
@@ -0,0 +1,39 @@
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+        
+    }
+    // reorder the queue everytime push operation
+    void push(int x) {
+        q.push(x);
+        int sz = q.size();
+        while(sz-- > 1){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int t = q.front();
+        q.pop();
+        return t;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
