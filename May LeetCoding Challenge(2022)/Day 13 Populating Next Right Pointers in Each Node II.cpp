ac5eaa1d42e73981b3c154d0106fda9625dcbb75
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-05-13 10:45:03 +0800

    Create Day 13 Populating Next Right Pointers in Each Node II.cpp

---
+++ May LeetCoding Challenge(2022)/Day 13 Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return root;
+        queue<Node*> q;
+        q.push(root);
+        int n = q.size();
+        
+        while(!q.empty()){
+            n = q.size();
+            Node* prev = NULL;
+            for(int i=0; i<n; i++){
+                Node* temp = q.front();
+                q.pop();
+                if (temp->left){
+                    q.push(temp->left);
+                }
+                if (temp->right){
+                    q.push(temp->right);
+                }
+                if (prev){
+                    prev->next = temp;                    
+                }
+                prev = temp;
+                
+            }
+        }
+        return root;
+    }
+};
