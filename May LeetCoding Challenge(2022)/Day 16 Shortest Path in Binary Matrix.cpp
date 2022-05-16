06adc746c727e29c71606308a8989ce3ebaf61dc
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-05-16 09:58:56 +0800

    Update Day 16 Shortest Path in Binary Matrix.cpp

--- May LeetCoding Challenge(2022)/Day 16 Shortest Path in Binary Matrix.cpp
+++ May LeetCoding Challenge(2022)/Day 16 Shortest Path in Binary Matrix.cpp
@@ -4,7 +4,6 @@ public:
         int n = grid.size();
         if (grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
         
-        //vector<vector<int>> moves = {{-1, -1}, {-1, 0}, {1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1,1}};
         int moves[8][2] = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1,-1},{1,-1},{-1,1}};
         queue<pair<int, int>> q;
         q.push({0, 0});
@@ -14,14 +13,13 @@ public:
             int sz = q.size();
             for(int i=0; i<sz; i++){
                 auto [x, y] = q.front(); q.pop();
-                if (grid[x][y] > 0) continue;
                 if (x == n-1 && y == n-1) return steps + 1;
-                grid[x][y] = 2;
                 
                 for(auto m : moves){
                     int new_x = x + m[0], new_y = y + m[1];
                     if (new_x < 0 || new_x >= n || new_y < 0 || new_y >= n || grid[new_x][new_y] > 0) continue;
                     q.push({new_x, new_y});
+                    grid[new_x][new_y] = 2;
                 }
             }
             steps++;
