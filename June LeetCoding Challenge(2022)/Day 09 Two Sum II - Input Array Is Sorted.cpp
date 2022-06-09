85b4ca66f2e0dbf63616c44d2110f0efe8acfe6c
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-06-09 10:05:58 +0800

    Create Day 09 Two Sum II - Input Array Is Sorted.cpp

---
+++ June LeetCoding Challenge(2022)/Day 09 Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int low = 0, high = numbers.size()-1;
+        while(low < high){
+            if (numbers[low] + numbers[high] == target){
+                return {low+1, high+1};
+            }else if (numbers[low] + numbers[high] > target){
+                high--;
+            }else{
+                low++;
+            }
+        }
+        return {-1, -1};
+    }
+};
