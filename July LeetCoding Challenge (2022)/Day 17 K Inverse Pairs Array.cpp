68e8d859a3b76d571a5c56bbea4b732f7a378ff5
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-07-17 13:51:20 +0800

    Create Day 17 K Inverse Pairs Array.cpp

---
+++ July LeetCoding Challenge (2022)/Day 17 K Inverse Pairs Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kInversePairs(int n, int k) {
+        vector<vector<long>> dp(n+1, vector<long>(k+1, 1));
+        vector<vector<long>> sum(n+1, vector<long>(k+1, 1));
+        
+        long MOD = 1e9 + 7;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=k; j++){
+                if (j < i) dp[i][j] = sum[i-1][j];
+                else dp[i][j] = (sum[i-1][j] - sum[i-1][j-i] + MOD) % MOD;
+                sum[i][j] = (sum[i][j-1] + dp[i][j] + MOD) % MOD;
+            }
+        }
+        return (dp[n][k] + MOD) % MOD;
+    }
+};
