bbbbd69e87da9d3615882a2afad30a17b3531ecc
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-08-05 20:10:39 +0800

    Create Day 05 Combination Sum IV.cpp

---
+++ August LeetCoding Challenge (2022)/Day 05 Combination Sum IV.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target + 1, 0); // number of combination for i
+        dp[0] = 1;
+        
+        for(int i=1; i<=target; i++){
+            for(int j=0; j<nums.size(); j++){
+                if (i - nums[j] >= 0){
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp.back();
+    }
+};
