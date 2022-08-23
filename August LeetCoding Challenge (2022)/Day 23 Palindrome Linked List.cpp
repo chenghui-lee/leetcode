a35aaf5346bfb15338ca1b44f78f6ab198ee3745
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-08-23 19:58:23 +0800

    Create Day 23 Palindrome Linked List.cpp

---
+++ August LeetCoding Challenge (2022)/Day 23 Palindrome Linked List.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> v;
+        while(head){
+            v.push_back(head->val);
+            head = head->next;
+        }
+        for(int i=0; i<v.size()/2; i++){
+            if (v[i] != v[v.size() - i - 1]) return false;
+        }
+        return true;
+    }
+};
