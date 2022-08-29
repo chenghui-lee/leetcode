f63ea08771f47679467dbbbaf503bfa64a872751
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-08-29 19:44:33 +0800

    Create Day 29 Number of Islands.cpp

---
+++ August LeetCoding Challenge (2022)/Day 29 Number of Islands.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+    int result = 0;
+    for (int i = 0; i<grid.size(); i++){
+    	for (int j = 0; j<grid[i].size(); j++){
+    		if (grid[i][j] == '1'){
+    			result++;
+    			islandSearch(grid, i , j);
+    		}
+    	}
+    }
+    return result;
+}
+    void islandSearch(vector<vector<char>> &grid, int i, int j){
+      if (i < 0 || j < 0 || i >=grid.size() || j>=grid[i].size() || grid[i][j] == '0'){
+        return;
+      }
+      grid[i][j] = '0';
+      islandSearch(grid, i+1, j); // up
+      islandSearch(grid, i-1, j); // down
+      islandSearch(grid, i, j+1); // right
+      islandSearch(grid, i, j-1); // left
+
+}
+};
