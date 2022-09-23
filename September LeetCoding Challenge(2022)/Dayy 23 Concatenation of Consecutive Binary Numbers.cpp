49f7ec5103c83249a3b4090ccb5b1c0837eaeadf
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-09-23 20:18:27 +0800

    Create Dayy 23 Concatenation of Consecutive Binary Numbers.cpp

---
+++ September LeetCoding Challenge(2022)/Dayy 23 Concatenation of Consecutive Binary Numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    
+    int concatenatedBinary(int n) {
+        int MOD = 1e9 + 7;
+        long res = 0;
+        for(int i=1; i<=n; i++){
+            int bitLen = log2(i)+1;
+            res = ((res << bitLen)+i)  % MOD;
+        }
+        return (int)res;
+    }
+};
