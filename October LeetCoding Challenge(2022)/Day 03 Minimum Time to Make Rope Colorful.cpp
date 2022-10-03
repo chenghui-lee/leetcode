21cb7417f00fff665831a25963ac1c45e1725520
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-10-03 20:20:34 +0800

    Create Day 03 Minimum Time to Make Rope Colorful.cpp

---
+++ October LeetCoding Challenge(2022)/Day 03 Minimum Time to Make Rope Colorful.cpp
@@ -0,0 +1,30 @@
+/*
+For a group of continuous same characters,
+we need to delete all the group but leave only one character.
+
+For each group of continuous same characters,
+we need cost = sum_cost(group) - max_cost(group)
+
+*/
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int n = colors.size();
+        int i =0, j = 0, res = 0;
+        while(i < n){
+            int curSum = 0;
+            int curMax = 0;
+            while(j < n && colors[i] == colors[j]){
+                curSum += neededTime[j];
+                curMax = max(curMax, neededTime[j++]);
+            }
+            if (i != j){
+                res += (curSum - curMax);
+                i = j;
+            }else{
+                i++;
+            }
+        }
+        return res;
+    }
+};Minimum Time to Make Rope Colorful
