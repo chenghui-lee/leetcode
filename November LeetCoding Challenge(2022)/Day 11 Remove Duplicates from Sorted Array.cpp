2f24e7f08cb46d45420951a7337c6d10b037a065
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2022-11-11 09:15:11 +0800

    Create Day 11 Remove Duplicates from Sorted Array.cpp

---
+++ November LeetCoding Challenge(2022)/Day 11 Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int j = 1;
+        for(int i=1; i<n; i++){
+            if (nums[i] == nums[i-1]) continue;
+            nums[j++] = nums[i];
+        }
+        return j;
+    }
+};
